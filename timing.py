710cfea3bb5eb459ddf8986453cf4021f263b695
LovepreetUppal <lovepreetuppal007@gmail.com>
2021-10-14 23:17:19 -0700

    Update timing.py
    added comments

--- timing.py
+++ timing.py
@@ -1,8 +1,10 @@
 import time
 def calculate_time(func):
+    # wrapper function that calls the parent function inside
     def wrap_time():
         before=time.time()
         func()
+        #Calculate via subtracting initial and final time
         print("Total time", time.time()-before)
     return wrap_time
 
