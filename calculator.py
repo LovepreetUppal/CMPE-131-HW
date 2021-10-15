9383db50806b416cbc2d6734a31c576af7f9e663
LovepreetUppal <lovepreetuppal007@gmail.com>
2021-10-14 23:46:51 -0700

    Update calculator.py
    fixed float division error

--- calculator.py
+++ calculator.py
@@ -12,7 +12,7 @@ def calculator(number1, number2, operator):
     if(operator=="*"):
         return(float(number1*number2))
     if(operator=="/"):
-        return(float(number1/number2))
+        return(number1/number2)
     if(operator=="//"):
         return(number1//number2)
     if(operator=="**"):
