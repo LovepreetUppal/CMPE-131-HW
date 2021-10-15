d3b3b522005f033b34c5c48e5c82d383ace6e3cf
LovepreetUppal <lovepreetuppal007@gmail.com>
2021-10-14 23:14:33 -0700

    Update swap.py
    added comments

--- swap.py
+++ swap.py
@@ -1,8 +1,9 @@
 def swap_last_item(list):
-     
-    first = list.pop(0)  
+    #Removes the first number 
+    first = list.pop(0) 
+    #Remove the last number
     last = list.pop(-1)
-     
+    #Insert the last number in position 0(front)
     list.insert(0, last) 
     list.append(first)  
      
